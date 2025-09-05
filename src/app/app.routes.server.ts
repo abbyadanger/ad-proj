b44b49ec66bbf772c880ab85a858c02437d04992
Abby Danger <abbyadanger@gmail.com>
2025-09-04 23:21:33 -0400

    More Updates

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,15 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  // {
-  //   path: 'blog/:filename',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     return [
-  //       { filename: 'first-blog-post.md' },
-  //     ];
-  //   }
-  // },
+  {
+    path: 'blog/:filename',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { filename: 'first-blog-post.md' },
+      ];
+    }
+  },
   {
       path: '**',
     renderMode: RenderMode.Prerender
