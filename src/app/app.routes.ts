59edef4d43c2c07f8fcefabc0e2801589bb3c68e
Abby Danger <abbyadanger@gmail.com>
2025-09-05 22:55:49 -0400

    Again

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
-import { BlogList } from './blog-list/blog-list';
 import { Home } from './home/home';
+import { BlogList } from './blog-list/blog-list';
 
 export const routes: Routes = [
+  { path: '', component: Home, pathMatch: 'full' },
   { path: 'blog', component: BlogList },
-  { path: '', component: Home, pathMatch: 'full' }, // root just shows empty/home
+  { path: '**', redirectTo: '' }
 ];
