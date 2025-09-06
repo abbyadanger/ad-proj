bb5effd136760caf45bd2fcf3b4af009188e2ea7
Abby Danger <abbyadanger@gmail.com>
2025-09-05 22:15:27 -0400

    New Routing Thing

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
+import { APP_BASE_HREF } from '@angular/common';
+
+const isProd = location.hostname.includes('github.io');
 
 bootstrapApplication(App, {
-  providers: [provideRouter(routes, withHashLocation())]
+  providers: [
+    provideRouter(routes, withHashLocation()),
+    { provide: APP_BASE_HREF, useValue: isProd ? '/ad-proj/' : '/' }
+  ]
 });
