e2a4a1aa165c1df267ab5a044f4f9694a5897a40
Abby Danger <abbyadanger@gmail.com>
2025-09-05 20:54:46 -0400

    Update Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { BlogList } from './blog-list/blog-list';
 import { Home } from './home/home';
 
 export const routes: Routes = [
-  { path: 'blog-list', component: BlogList },
+  { path: '/ad-proj/blog-list', component: BlogList },
   { path: '', component: Home, pathMatch: 'full' }, // root just shows empty/home
 ];
