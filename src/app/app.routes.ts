17ae88b3851d3c5ef4bab8ae9179d1837ec58c74
Abby Danger <abbyadanger@gmail.com>
2025-09-05 21:47:23 -0400

    Does the routing work?

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { BlogList } from './blog-list/blog-list';
 import { Home } from './home/home';
 
 export const routes: Routes = [
-  { path: '/ad-proj/blog-list', component: BlogList },
+  { path: 'blog', component: BlogList },
   { path: '', component: Home, pathMatch: 'full' }, // root just shows empty/home
 ];
