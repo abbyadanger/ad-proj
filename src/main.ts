43673e287e1f03b3b689fecbe6b5050c03b8c4a8
Abby Danger <abbyadanger@gmail.com>
2025-09-15 15:31:06 -0500

    Updating Server.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { routes } from './app/app.routes';
-import { environment } from '././environments/environment';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(routes, withHashLocation()),
-  ]
-});
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
